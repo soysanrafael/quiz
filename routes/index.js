9d43ea3f31ee23daac88ccfe3694dab7f1357795
soysanrafael <soysanrafael@gmail.com>
2015-07-28 17:30:42 +0200

    Añadida página author

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index.ejs', { title: 'Quiz' });
 });
-
+/* GET author page */
+router.get('/author', function(req, res) {
+  res.render('author.ejs');
+});
 /* GET question page. */
 router.get('/quizes/question', quizController.question);
 /* GET answer page. */
