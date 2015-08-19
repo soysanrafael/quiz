bc5029c389f0ac417d92acd9a38ee41d35ac2f6f
soysanrafael <soysanrafael@gmail.com>
2015-08-19 12:44:00 +0200

    Autenticacion y sesion

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
@@ -27,10 +28,20 @@ app.use(bodyParser.json());
 //el objeto req.body.quiz, si quitamos el parámetro de configuración {extended: false}
 //que express-generator incluyó cuando generó el proyecto.
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+  //guardar path en session.redir para despues de loginRequired
+  if (!req.path.match(/\/login|\/logout|\/user/)){
+    req.session.redir = req.path;
+  }
+  // Hacer visible req.session en las vistas
+  res.locals.session = req.session;
+  next();
+})
 app.use('/', routes);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -23,6 +24,11 @@ router.get('/quizes/new', quizController.new);
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.loadId);  // autoload :quizId
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
